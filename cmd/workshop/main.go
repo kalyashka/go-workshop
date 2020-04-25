2ad58f35a37ddf3a95a2f29476f795b2d17b8e3d
Nikolay Grom <kalyashka@gmail.com>
2020-04-25 12:58:34 +0300

    add config parsing

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -5,17 +5,25 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/ilyakaznacheev/cleanenv"
 
+	"workshop/internal/config"
 	"workshop/internal/handler"
 )
 
 func main() {
+	cfg := config.Server{}
+	err := cleanenv.ReadConfig("config.yml", &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	h := handler.NewHandler()
 	r := chi.NewRouter()
 	r.Get("/hello", h.Hello)
 
 	log.Print("Starting server")
-	err := http.ListenAndServe(":8080", r)
+	err = http.ListenAndServe(":8080", r)
 	log.Fatal(err)
 
 	log.Print("shutting down")
