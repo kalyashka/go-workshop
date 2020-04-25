ec26f23f4917ff902464e00fc0dd61240dd30768
Nikolay Grom <kalyashka@gmail.com>
2020-04-25 22:44:39 +0300

    jokes+config

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module workshop
 go 1.14
 
 require (
-	github.com/go-chi/chi v4.1.1+incompatible // indirect
-	github.com/ilyakaznacheev/cleanenv v1.2.2 // indirect
+	github.com/go-chi/chi v4.1.1+incompatible
+	github.com/ilyakaznacheev/cleanenv v1.2.2
 )
---
+++ internal/handler/handler_test.go
@@ -0,0 +1,30 @@
+package handler
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestHandler_Hello(t *testing.T) {
+	type fields struct {
+		jokeClient api.Client
+	}
+	type args struct {
+		w http.ResponseWriter
+		r *http.Request
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := &Handler{
+				jokeClient: tt.fields.jokeClient,
+			}
+		})
+	}
+}
